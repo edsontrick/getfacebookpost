4704edf7ba2f6ddc7b00fa1b558751bda0443c49
Patrick <p14n@live.com>
2017-05-11 17:16:34 -0300

    Alteração

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -1,5 +1,28 @@
 $( document ).ready(function() {
+	console.log();
+
+	$("#sortear").click(function(){
+		$("#ganhador").html("GANHADOR: " + shuffle($(".item")).slice(0, 1)[0].textContent);
+	});
 });
 
+
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
 
 
