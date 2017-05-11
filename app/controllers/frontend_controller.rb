a24d43045a28a32a2109c26a65c77fb2973d9b9f
Patrick <p14n@live.com>
2017-05-11 16:21:23 -0300

    ada

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -4,7 +4,7 @@ class FrontendController < ApplicationController
 		require 'uri'
 		require 'json'
 
-		uri = URI.parse("https://graph.facebook.com/v2.3/#{params[:idpost]}/sharedposts?fields=from&limit=9999&access_token=EAACEdEose0cBACMQ8P0vHjAbuII8DvxqqxAaI4cEooog1tCopcrllHnogGyTQlFtZCyvCgZBHKGDk7WQHpeSBY5BPr1ycLDDiP8lOqA1SQj3CiHuTzvsrjjPa9JJmVkgpo2DbeInPRQtZBClz7K0Q7EhIkLvp6B5JCxBQM8jRcNp4MtWyIC")
+		uri = URI.parse("https://graph.facebook.com/v2.3/#{params[:idpost]}/sharedposts?fields=from&limit=9999&access_token=#{params[:token]}")
 		response = Net::HTTP.get_response(uri)
 		@json = JSON.parse(response.body)
 	end
