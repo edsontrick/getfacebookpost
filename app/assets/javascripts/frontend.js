251f8bec0137391228bc379e190943fea01d2499
Patrick <p14n@live.com>
2017-05-11 16:16:44 -0300

    ada

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -1,9 +1,4 @@
 $( document ).ready(function() {
-	$("#id_post").on('change keydown paste input', function(){
-		$.get("1026519994145976", function(data, status){
-			console.log(data);
-		});
-	});
 });
 
 
--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -4,7 +4,7 @@ class FrontendController < ApplicationController
 		require 'uri'
 		require 'json'
 
-		uri = URI.parse("https://graph.facebook.com/v2.3/#{params[:idpost]}/sharedposts?fields=from&limit=9999&access_token=EAACEdEose0cBAFQOCGEnk47GyEJiFku3JhR2O0Kzk0buqyvNTYf63ZCLvZBapSnO94HhD1AYBq5X2ZABm7qYhWilbZAssVNnT8SxR11AgQkNe3GD1MjkvUSrsFq2S4qWL1taLnjt58MhwVMBTkT2ox7IfXKHDPpDTyVjDV5WfnzHYpfp5nDp")
+		uri = URI.parse("https://graph.facebook.com/v2.3/#{params[:idpost]}/sharedposts?fields=from&limit=9999&access_token=EAACEdEose0cBACMQ8P0vHjAbuII8DvxqqxAaI4cEooog1tCopcrllHnogGyTQlFtZCyvCgZBHKGDk7WQHpeSBY5BPr1ycLDDiP8lOqA1SQj3CiHuTzvsrjjPa9JJmVkgpo2DbeInPRQtZBClz7K0Q7EhIkLvp6B5JCxBQM8jRcNp4MtWyIC")
 		response = Net::HTTP.get_response(uri)
 		@json = JSON.parse(response.body)
 	end
